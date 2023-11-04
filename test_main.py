284b8bcc14c088d12784c7a1c063fec0f653f6b9
jaxonyue <70416390+jaxonyue@users.noreply.github.com>
2023-11-04 23:06:04 +0000

    test

--- test_main.py
+++ test_main.py
@@ -1,4 +1,5 @@
 import pytest
+from pyspark.sql.session import SparkSession
 from mylib.lib import initiate_spark_session, read_dataset
 from mylib.lib import describe, handle_missing_values
 
@@ -10,21 +11,21 @@ def spark_session():
     session.stop()
 
 
-def test_data_loading(spark_session):
+def test_data_loading(spark_session: SparkSession):
     data_file_path = "data/Development of Average Annual Wages_1.csv"
     df = read_dataset(spark_session, data_file_path)
     assert df is not None
     assert df.count() > 0
 
 
-def test_data_describe(spark_session):
+def test_data_describe(spark_session: SparkSession):
     data_file_path = "data/Development of Average Annual Wages_1.csv"
     df = read_dataset(spark_session, data_file_path)
     description_data = describe(df)
     assert description_data is not None
 
 
-def test_handle_missing_values(spark_session):
+def test_handle_missing_values(spark_session: SparkSession):
     data_file_path = "data/Development of Average Annual Wages_1.csv"
     df = read_dataset(spark_session, data_file_path)
     description_data = handle_missing_values(df)
