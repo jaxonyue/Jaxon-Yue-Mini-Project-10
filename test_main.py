4f5076ecbe8da5e6e1552d1fae4fda70992b2faa
jaxonyue <70416390+jaxonyue@users.noreply.github.com>
2023-11-04 22:58:01 +0000

    a

--- test_main.py
+++ test_main.py
@@ -1,18 +1,27 @@
 import pytest
-from mylib.lib import initiate_spark_session, read_dataset, describe
+from mylib.lib import initiate_spark_session, read_dataset, describe, handle_missing_values
 
 @pytest.fixture(scope="session")
 def spark_session():
-    return initiate_spark_session("Test Country Wages Analysis")
+    session = initiate_spark_session("Test Country Wages Analysis")
+    yield session
+    session.stop()
 
 # The fixture "spark_session" is automatically used in tests that list it as an argument
 def test_data_loading(spark_session):
-    data_path = "data/Development of Average Annual Wages_1.csv"
-    df = read_dataset(spark_session, data_path)
+    data_file_path = "data/Development of Average Annual Wages_1.csv"
+    df = read_dataset(spark_session, data_file_path)
     assert df is not None
     assert df.count() > 0
 
 def test_data_describe(spark_session):
-    df = read_dataset(spark_session, "data/Development of Average Annual Wages_1.csv")
+    data_file_path = "data/Development of Average Annual Wages_1.csv"
+    df = read_dataset(spark_session, data_file_path)
     description_data = describe(df)
+    assert description_data is not None
+
+def test_handle_missing_values(spark_session):
+    data_file_path = "data/Development of Average Annual Wages_1.csv"
+    df = read_dataset(spark_session, data_file_path)
+    description_data = handle_missing_values(df)
     assert description_data is not None
