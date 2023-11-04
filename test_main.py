19574a34aa75c0a646499ee8ed03ec13641b2e49
jaxonyue <70416390+jaxonyue@users.noreply.github.com>
2023-11-04 23:00:50 +0000

    a

--- test_main.py
+++ test_main.py
@@ -1,5 +1,7 @@
 import pytest
-from mylib.lib import initiate_spark_session, read_dataset, describe, handle_missing_values
+from mylib.lib import initiate_spark_session, read_dataset
+from mylib.lib import describe, handle_missing_values
+
 
 @pytest.fixture(scope="session")
 def spark_session():
@@ -7,18 +9,20 @@ def spark_session():
     yield session
     session.stop()
 
-# The fixture "spark_session" is automatically used in tests that list it as an argument
+
 def test_data_loading(spark_session):
     data_file_path = "data/Development of Average Annual Wages_1.csv"
     df = read_dataset(spark_session, data_file_path)
     assert df is not None
     assert df.count() > 0
 
+
 def test_data_describe(spark_session):
     data_file_path = "data/Development of Average Annual Wages_1.csv"
     df = read_dataset(spark_session, data_file_path)
     description_data = describe(df)
     assert description_data is not None
+
 
 def test_handle_missing_values(spark_session):
     data_file_path = "data/Development of Average Annual Wages_1.csv"
